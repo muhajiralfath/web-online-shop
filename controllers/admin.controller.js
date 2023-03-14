62359f123f67211951a36cb99a0d3074bcd1d16a
muhajiralfath <muhammadmuhajiralfath@gmail.com>
2023-03-14 23:32:18 +0700

    Add product Model & fetch product

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,13 +1,31 @@
-function getProducts(req, res) {
-  res.render('admin/products/all-products');
+const Product = require('../models/product.model');
+
+async function getProducts(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    res.render('admin/products/all-products', { products: products });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
 function getNewProduct(req, res) {
   res.render('admin/products/new-product');
 }
 
-function createNewProduct(req, res) {
-  
+async function createNewProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   res.redirect('/admin/products');
 }
@@ -15,5 +33,5 @@ function createNewProduct(req, res) {
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
-  createNewProduct: createNewProduct
+  createNewProduct: createNewProduct,
 };
